0eaa2995db407f5f656adf0c87c0d1051150e790
Padam <sandhp7@gmail.com>
2018-06-11 13:39:56 -0400

    completed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,38 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "../Style/HeaderStyle.css";
 
-export class Header extends React.Component {
+ class HeaderClass extends React.Component<any,any> {
+    public renderLinks() {
+        if(this.props.authenticaticated) {
+            return (
+                <div>
+                    <Link to="/signout"> Sign Out </Link>
+                    <Link to="/feature"> Feature </Link>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <Link to="/signup"> Signup </Link>
+                    <Link to="/signin"> Signin </Link>
+                </div>
+            )
+        };
+    }
     public render() {
         return (
-            <div>
+            <div className="header">
             <Link to="/">Redux Auth </Link>
-            <Link to="/signup"> Signup </Link>
-            <Link to="/signin"> Signin </Link>
-            <Link to="/signout"> Sign Out </Link>
-            <Link to="/feature"> Feature </Link>
+            {this.renderLinks()}
             </div>
         );
     }
 }
+function mapStateToProps(state:any) {
+    return { authenticaticated: state.authentication          }
+  }
+
+  export const Header = connect(mapStateToProps)(HeaderClass);
 // Header is always Visible!!
